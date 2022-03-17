94ff17696f31b2561972ca3c368837fdc8448da7
vladshatilov <shatilovvlad@mail.ru>
2022-03-17 18:33:23 +0300

    added readme

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from auth_user.views import UserCreateView
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', UserCreateView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view()),
 ]
